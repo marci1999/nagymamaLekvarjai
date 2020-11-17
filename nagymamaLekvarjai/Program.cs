63db95ea12433f137d5c5e53c0ab8bb322de1abe
jakus marci <jakus.marton@gmail.com>
2020-11-17 21:39:03 +0100

    ha az sqlite müköde ez jo lenen

--- nagymamaLekvarjai/Program.cs
+++ nagymamaLekvarjai/Program.cs
@@ -19,22 +19,74 @@ namespace nagymamaLekvarjai
 
                 createComm.CommandText = @"
 CREATE TABLE IF NOT EXISTS uvegek(
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    meret INT,
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    meret INTEGER,
     tipus VACHAR(1000) NOT NULL
 )
-
 ";
                 createComm.ExecuteNonQuery();
+
+                Console.WriteLine("adjon megy egy üvegmétetet.");
+                var uvegMeret = Console.ReadLine();
+                Console.WriteLine("adjon megy egy lekvártipust.");
+                var lekvarTipus = Console.ReadLine();
 
                 var insertComm = conn.CreateCommand();
                 insertComm.CommandText = @"
 INSERT INTO uvegek (meret, tipus)
-VALUES (500, 'szilva')
+VALUES ('@uvegMeret', '@lekvarTipus')
 ";
+                insertComm.Parameters.AddWithValue("@uvegMeret", uvegMeret);
+                insertComm.Parameters.AddWithValue("@lekvarTipus", lekvarTipus);
                 insertComm.ExecuteNonQuery();
 
-                
+                var selectComm = conn.CreateCommand();
+
+                createComm.CommandText = @"
+SELECT meret, tipus 
+FROM uvegek
+WHERE 1
+";
+                using (var reader = createComm.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        int emeret = reader.GetInt32(0);
+                        string etipus = reader.GetString(1);
+                        Console.WriteLine("{1}-{2}",emeret,etipus);
+                    }
+                }
+
+                createComm.CommandText = @"
+SELECT SUM(meret)
+FROM uvegek
+WHERE 1
+";
+                using (var reader = createComm.ExecuteReader())
+                {
+                    var eszam = reader.GetInt32(0);
+                }
+
+                createComm.CommandText = @"
+SELECT tipus, 
+FROM uvegek
+WHERE 1
+";
+                using (var reader = createComm.ExecuteReader())
+                {
+                    var eszam = reader.GetInt32(0);
+                }
+
+                createComm.CommandText = @"
+SELECT AVG(SUM(meret))
+FROM uvegek
+WHERE 1
+";
+                using (var reader = createComm.ExecuteReader())
+                {
+                    var eszam = reader.GetInt32(0);
+                }
+
 
                 Console.ReadLine();
             }
