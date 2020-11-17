aeb54921536fea0af521839967ea84169104e90a
jakus marci <jakus.marton@gmail.com>
2020-11-17 19:51:39 +0100

    adatbázis

--- nagymamaLekvarjai/Program.cs
+++ nagymamaLekvarjai/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SQLite;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,33 @@ namespace nagymamaLekvarjai
     {
         static void Main(string[] args)
         {
+            using (var conn = new SQLiteConnection("Data Source=mydb.db"))
+            {
+                conn.Open();
 
+                var createComm = conn.CreateCommand();
+
+                createComm.CommandText = @"
+CREATE TABLE IF NOT EXISTS uvegek(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    meret INT,
+    tipus VACHAR(1000) NOT NULL
+)
+
+";
+                createComm.ExecuteNonQuery();
+
+                var insertComm = conn.CreateCommand();
+                insertComm.CommandText = @"
+INSERT INTO uvegek (meret, tipus)
+VALUES (500, 'szilva')
+";
+                insertComm.ExecuteNonQuery();
+
+                
+
+                Console.ReadLine();
+            }
         }
     }
 }
